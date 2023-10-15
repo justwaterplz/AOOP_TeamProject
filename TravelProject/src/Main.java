19216f4373c6250bb003550efaece147e3bcb862
byungmeo <wlsrlfvkr@gmail.com>
2023-10-15 22:18:00 +0900

    Add Main With JDBC Lib

---
+++ TravelProject/src/Main.java
@@ -0,0 +1,46 @@
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String connectionString = "jdbc:mysql://localhost:3306/main";
+        String userName = "root";
+        String password = "root";
+
+        try {
+            // JDBC 드라이버 로드
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 데이터베이스 연결
+            Connection connection = DriverManager.getConnection(connectionString, userName, password);
+
+            // SQL 쿼리 실행
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM 관광지");
+
+            // 결과 출력
+            while (resultSet.next()) {
+                // 각 열의 값을 가져오기 (여기서는 예를 들어 1번째와 2번째 열의 값을 가져옵니다)
+                int 관광지ID = resultSet.getInt(1);
+                int 코스ID = resultSet.getInt(2);
+                String 지역ID = resultSet.getString(3);
+                String 관광지명 = resultSet.getString(4);
+                BigDecimal 경도 = resultSet.getBigDecimal(5);
+                BigDecimal 위도 = resultSet.getBigDecimal(6);
+                int 코스순서 = resultSet.getInt(7);
+                int 이동시간 = resultSet.getInt(8);
+                String 실내구분 = resultSet.getString(9);
+                String 테마분류 = resultSet.getString(10);
+                System.out.println(관광지ID + " " + 코스ID + " " + 지역ID + " " + 관광지명 + " " + 경도 + " " + 위도 + " " + 코스순서 + " " + 이동시간 + " " + 실내구분 + " " + 테마분류);
+            }
+
+            // 자원 해제
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+    }
+}
