2ef7d7c777d8ec10cc848d58c1081a1f24ea5d0a
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-10-20 19:03:58 +0900

    Service에 관광지들 찾는 함수 추가

--- TravelProject/src/Main/Service/MainService.java
+++ TravelProject/src/Main/Service/MainService.java
@@ -1,4 +1,27 @@
 package Main.Service;
 
+import Main.Model.Model관광지;
+import Main.Repository.MainRepository;
+
+import java.util.ArrayList;
+
 public class MainService {
+    private final MainRepository mainRepository;
+
+    public MainService() {
+        this. mainRepository = new MainRepository();
+    }
+
+    public ArrayList<Model관광지> findTouristSpots(String type, String data) {
+        ArrayList<Model관광지> model관광지 = new ArrayList<>();
+        if(type.equals("검색")){
+
+        } else if (type.equals("테마")) {
+            model관광지 = mainRepository.get관광지ListByTheme(data);
+        } else if (type.equals("실내외")) {
+            model관광지 = mainRepository.get관광지ListByIndoorOutdoor(data);
+        }
+        return model관광지;
+    }
+
 }
