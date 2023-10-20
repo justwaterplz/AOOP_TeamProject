f15a60a2a4a739a27931d64c91a381b7c66633a9
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-10-20 19:04:27 +0900

    MainView의 버튼리스너부분 Map을 이용해 ListView에 넘기도록 수정

--- TravelProject/src/Main/Page/MainView.java
+++ TravelProject/src/Main/Page/MainView.java
@@ -3,6 +3,8 @@ package Main.Page;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainView extends PageBase {
     private JPanel mainPanel;
@@ -36,26 +38,38 @@ public class MainView extends PageBase {
         @Override
         public void actionPerformed(ActionEvent e) {
             JButton clickedButton = (JButton) e.getSource();
+            Map<String,String> filterData = new HashMap<>();
 
             if (clickedButton == searchButton) {  // "검색"
-                listener.onPageChanged(new ListView(listener).getMainPanel());
+                filterData.put("FilterType","검색");
+                filterData.put("FilterData",searchTextField.getText().toString());
             } else if (clickedButton == th01Button) {   // "문화예술"
-
+                filterData.put("FilterType","테마");
+                filterData.put("FilterData","TH01");
             } else if (clickedButton == th02Button) {   // "자연힐링"
-
+                filterData.put("FilterType","테마");
+                filterData.put("FilterData","TH02");
             } else if (clickedButton == th03Button) {   // "캠핑스포츠"
-
+                filterData.put("FilterType","테마");
+                filterData.put("FilterData","TH03");
             } else if (clickedButton == th04Button) {   // "종교역사전통"
-
+                filterData.put("FilterType","테마");
+                filterData.put("FilterData","TH04");
             } else if (clickedButton == th05Button) {   // "쇼핑놀이"
-
+                filterData.put("FilterType","테마");
+                filterData.put("FilterData","TH05");
             } else if (clickedButton == th06Button) {   // "체험학습산업"
-
+                filterData.put("FilterType","테마");
+                filterData.put("FilterData","TH06");
             } else if (clickedButton == insideButton) { // "실내"
-
+                filterData.put("FilterType","실내외");
+                filterData.put("FilterData","실내");
             } else if (clickedButton == outsideButton) {  // "실외"
-
+                filterData.put("FilterType","실내외");
+                filterData.put("FilterData","실외");
             }
+
+            listener.onPageChanged(new ListView(listener,filterData).getMainPanel());
         }
     }
 
