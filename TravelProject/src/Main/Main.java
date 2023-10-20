c0e96e2c9c8b7845999643fd08d9450dfa023f2a
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-10-20 18:58:43 +0900

    Main에 테스트코드 제거

--- TravelProject/src/Main/Main.java
+++ TravelProject/src/Main/Main.java
@@ -8,11 +8,6 @@ public class Main {
         try {
             // JDBC 드라이버 로드
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // 테스트, 실제로는 응용단에서 Service 객체를 사용
-            MainRepository mainRepository = new MainRepository();
-            mainRepository.get관광지List();
-            mainRepository.get테마List();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
