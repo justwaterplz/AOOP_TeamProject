a364d63c561f4aac190849bc2a238cc0e814620d
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-10-20 19:05:55 +0900

    PageBase가 mainService를 가지도록 설정

--- TravelProject/src/Main/Page/PageBase.java
+++ TravelProject/src/Main/Page/PageBase.java
@@ -1,12 +1,17 @@
 package Main.Page;
 
 
+import Main.Repository.MainRepository;
+import Main.Service.MainService;
+
 import javax.swing.*;
 
 abstract public class PageBase {
     protected PageChangeListener listener;
+    protected final MainService mainService;
 
     public PageBase(PageChangeListener listener) {
+        this.mainService = new MainService();
         this.listener = listener;
     }
 
