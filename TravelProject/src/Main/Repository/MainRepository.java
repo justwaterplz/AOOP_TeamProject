1f240fb2cbb9635c596c2cc3ff271a23532e639f
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-10-22 15:13:22 +0900

    Search기준 필터링 추가

--- TravelProject/src/Main/Repository/MainRepository.java
+++ TravelProject/src/Main/Repository/MainRepository.java
@@ -141,7 +141,7 @@ public class MainRepository {
         }
     }
 
-    public ArrayList<Model관광지> get관광지ListByIndoorOutdoor(String data) {
+    public ArrayList<Model관광지> get관광지ListBySearch(String data) {
         Connection conn = pool.getConnection();
         if(conn == null) {
             System.out.println("Failed Get Connection");
@@ -151,8 +151,8 @@ public class MainRepository {
         // SQL 쿼리 실행
         PreparedStatement statement = null;
         try {
-            statement = conn.prepareStatement("SELECT * FROM 관광지 WHERE 실내구분 = ?");
-            statement.setString(1, data); // 첫 번째 파라미터에 theme을 설정합니다.
+            statement = conn.prepareStatement( "SELECT * FROM 관광지 WHERE 관광지명 LIKE ?");
+            statement.setString(1, "%" + data + "%");
             ResultSet resultSet = statement.executeQuery();
 
             // 결과 출력 및 저장
@@ -186,4 +186,51 @@ public class MainRepository {
             pool.releaseConnection(conn);
         }
     }
+
+    public ArrayList<Model관광지> get관광지ListByIndoorOutdoor(String theme) {
+        Connection conn = pool.getConnection();
+        if(conn == null) {
+            System.out.println("Failed Get Connection");
+            return null;
+        }
+
+        // SQL 쿼리 실행
+        PreparedStatement statement = null;
+        try {
+            statement = conn.prepareStatement("SELECT * FROM 관광지 WHERE 실내구분 = ?");
+            statement.setString(1, theme); // 첫 번째 파라미터에 theme을 설정합니다.
+            ResultSet resultSet = statement.executeQuery();
+
+            // 결과 출력 및 저장
+            ArrayList<Model관광지> 관광지List = new ArrayList<>();
+            while (resultSet.next()) {
+                // 각 열의 값을 가져오기 (여기서는 예를 들어 1번째와 2번째 열의 값을 가져옵니다)
+                int 관광지ID = resultSet.getInt(1);
+                int 코스ID = resultSet.getInt(2);
+                String 지역ID = resultSet.getString(3);
+                String 관광지명 = resultSet.getString(4);
+                BigDecimal 경도 = resultSet.getBigDecimal(5);
+                BigDecimal 위도 = resultSet.getBigDecimal(6);
+                int 코스순서 = resultSet.getInt(7);
+                int 이동시간 = resultSet.getInt(8);
+                String 실내구분 = resultSet.getString(9);
+                String 테마분류 = resultSet.getString(10);
+                Model관광지 관광지 = new Model관광지(관광지ID, 코스ID, 지역ID, 관광지명, 경도, 위도, 코스순서, 이동시간, 실내구분, 테마분류);
+                관광지List.add(관광지);
+                System.out.println(관광지);
+            }
+
+            // 자원 해제
+            resultSet.close();
+            statement.close();
+
+            return 관광지List;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.releaseConnection(conn);
+        }
+    }
+
 }
