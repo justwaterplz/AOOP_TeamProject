47840332cbd49f47e1df75c803b7aa49e97cb1b6
byungmeo <wlsrlfvkr@gmail.com>
2023-11-05 18:07:09 +0900

    즐겨찾기 기능 추가를 위한 DB 스키마 및 Repository 수정

--- TravelProject/src/Main/Repository/MainRepository.java
+++ TravelProject/src/Main/Repository/MainRepository.java
@@ -1,7 +1,6 @@
 package Main.Repository;
+import Main.Model.*;
 import Main.Module.ConnectionPool;
-import Main.Model.Model관광지;
-import Main.Model.Model테마;
 
 import java.math.BigDecimal;
 import java.sql.*;
@@ -233,4 +232,151 @@ public class MainRepository {
         }
     }
 
+    public ArrayList<Model코스> get코스List() {
+        Connection conn = pool.getConnection();
+        if(conn == null) {
+            System.out.println("Failed Get Connection");
+            return null;
+        }
+
+        // SQL 쿼리 실행
+        Statement statement = null;
+        try {
+            statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM 코스");
+
+            // 결과 출력 및 저장
+            ArrayList<Model코스> 코스List = new ArrayList<>();
+            while (resultSet.next()) {
+                String 코스ID = resultSet.getString(1);
+                Model코스 코스 = new Model코스(코스ID);
+                코스List.add(코스);
+            }
+
+            // 자원 해제
+            resultSet.close();
+            statement.close();
+
+            return 코스List;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.releaseConnection(conn);
+        }
+    }
+
+    public ArrayList<Model선호코스> get선호코스List() {
+        Connection conn = pool.getConnection();
+        if(conn == null) {
+            System.out.println("Failed Get Connection");
+            return null;
+        }
+
+        // SQL 쿼리 실행
+        Statement statement = null;
+        try {
+            statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM 선호코스");
+
+            // 결과 출력 및 저장
+            ArrayList<Model선호코스> 선호코스List = new ArrayList<>();
+            while (resultSet.next()) {
+                String 코스ID = resultSet.getString(1);
+                Model선호코스 선호코스 = new Model선호코스(코스ID);
+                선호코스List.add(선호코스);
+            }
+
+            // 자원 해제
+            resultSet.close();
+            statement.close();
+
+            return 선호코스List;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.releaseConnection(conn);
+        }
+    }
+
+    public ArrayList<Model선호관광지> get선호관광지List() {
+        Connection conn = pool.getConnection();
+        if(conn == null) {
+            System.out.println("Failed Get Connection");
+            return null;
+        }
+
+        // SQL 쿼리 실행
+        Statement statement = null;
+        try {
+            statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM 선호관광지");
+
+            // 결과 출력 및 저장
+            ArrayList<Model선호관광지> 선호관광지List = new ArrayList<>();
+            while (resultSet.next()) {
+                String 관광지ID = resultSet.getString(1);
+                Model선호관광지 선호관광지 = new Model선호관광지(관광지ID);
+                선호관광지List.add(선호관광지);
+            }
+
+            // 자원 해제
+            resultSet.close();
+            statement.close();
+
+            return 선호관광지List;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.releaseConnection(conn);
+        }
+    }
+
+    public ArrayList<Model관광지> get관광지ListBy코스(String _코스ID) {
+        Connection conn = pool.getConnection();
+        if(conn == null) {
+            System.out.println("Failed Get Connection");
+            return null;
+        }
+
+        // SQL 쿼리 실행
+        PreparedStatement statement = null;
+        try {
+            statement = conn.prepareStatement("SELECT * FROM 관광지 WHERE 코스ID = ?");
+            statement.setString(1, _코스ID);
+            ResultSet resultSet = statement.executeQuery();
+
+            // 결과 출력 및 저장
+            ArrayList<Model관광지> 관광지List = new ArrayList<>();
+            while (resultSet.next()) {
+                // 각 열의 값을 가져오기 (여기서는 예를 들어 1번째와 2번째 열의 값을 가져옵니다)
+                int 관광지ID = resultSet.getInt(1);
+                int 코스ID = resultSet.getInt(2);
+                String 지역ID = resultSet.getString(3);
+                String 관광지명 = resultSet.getString(4);
+                BigDecimal 경도 = resultSet.getBigDecimal(5);
+                BigDecimal 위도 = resultSet.getBigDecimal(6);
+                int 코스순서 = resultSet.getInt(7);
+                int 이동시간 = resultSet.getInt(8);
+                String 실내구분 = resultSet.getString(9);
+                String 테마분류 = resultSet.getString(10);
+                Model관광지 관광지 = new Model관광지(관광지ID, 코스ID, 지역ID, 관광지명, 경도, 위도, 코스순서, 이동시간, 실내구분, 테마분류);
+                관광지List.add(관광지);
+                System.out.println(관광지);
+            }
+
+            // 자원 해제
+            resultSet.close();
+            statement.close();
+
+            return 관광지List;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.releaseConnection(conn);
+        }
+    }
 }
