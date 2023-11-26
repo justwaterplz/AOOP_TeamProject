9f751ee79f652c74c616373026826e1ea65df00f
byungmeo <wlsrlfvkr@gmail.com>
2023-11-26 18:40:22 +0900

    Repository 일부 수정

--- TravelProject/src/Main/Model/Model코스정보.java
+++ TravelProject/src/Main/Model/Model코스정보.java
@@ -1,31 +1,18 @@
 package Main.Model;
 
+import java.util.ArrayList;
+
 public class Model코스정보 {
-    int 관광지ID;
     int 코스ID;
-    int 코스순서;
-    int 이동시간;
+    ArrayList<Integer> 관광지ID목록;
+    ArrayList<Integer> 코스순서목록;
+    ArrayList<Integer> 이동시간목록;
 
-    public Model코스정보(int 관광지ID, int 코스ID, int 코스순서, int 이동시간) {
-        this.관광지ID = 관광지ID;
+    public Model코스정보(int 코스ID, ArrayList<Integer> 관광지ID목록, ArrayList<Integer> 코스순서목록, ArrayList<Integer> 이동시간목록) {
         this.코스ID = 코스ID;
-        this.코스순서 = 코스순서;
-        this.이동시간 = 이동시간;
-    }
-
-    public Model코스정보(int 관광지ID, int 코스ID) {
-        this.관광지ID = 관광지ID;
-        this.코스ID = 코스ID;
-        코스순서 = 0;
-        이동시간 = 0;
-    }
-
-    public int get관광지ID() {
-        return 관광지ID;
-    }
-
-    public void set관광지ID(int 관광지ID) {
-        this.관광지ID = 관광지ID;
+        this.관광지ID목록 = 관광지ID목록;
+        this.코스순서목록 = 코스순서목록;
+        this.이동시간목록 = 이동시간목록;
     }
 
     public int get코스ID() {
@@ -36,19 +23,27 @@ public class Model코스정보 {
         this.코스ID = 코스ID;
     }
 
-    public int get코스순서() {
-        return 코스순서;
+    public ArrayList<Integer> get관광지ID목록() {
+        return 관광지ID목록;
     }
 
-    public void set코스순서(int 코스순서) {
-        this.코스순서 = 코스순서;
+    public void set관광지ID목록(ArrayList<Integer> 관광지ID목록) {
+        this.관광지ID목록 = 관광지ID목록;
     }
 
-    public int get이동시간() {
-        return 이동시간;
+    public ArrayList<Integer> get코스순서목록() {
+        return 코스순서목록;
     }
 
-    public void set이동시간(int 이동시간) {
-        this.이동시간 = 이동시간;
+    public void set코스순서목록(ArrayList<Integer> 코스순서목록) {
+        this.코스순서목록 = 코스순서목록;
+    }
+
+    public ArrayList<Integer> get이동시간목록() {
+        return 이동시간목록;
+    }
+
+    public void set이동시간목록(ArrayList<Integer> 이동시간목록) {
+        this.이동시간목록 = 이동시간목록;
     }
 }
--- TravelProject/src/Main/Repository/MainRepository.java
+++ TravelProject/src/Main/Repository/MainRepository.java
@@ -31,17 +31,15 @@ public class MainRepository {
             ArrayList<Model관광지> 관광지List = new ArrayList<>();
             while (resultSet.next()) {
                 // 각 열의 값을 가져오기 (여기서는 예를 들어 1번째와 2번째 열의 값을 가져옵니다)
-                int 관광지ID = resultSet.getInt(1);
-                int 코스ID = resultSet.getInt(2);
-                String 지역ID = resultSet.getString(3);
-                String 관광지명 = resultSet.getString(4);
-                BigDecimal 경도 = resultSet.getBigDecimal(5);
-                BigDecimal 위도 = resultSet.getBigDecimal(6);
-                int 코스순서 = resultSet.getInt(7);
-                int 이동시간 = resultSet.getInt(8);
-                String 실내구분 = resultSet.getString(9);
-                String 테마분류 = resultSet.getString(10);
-                Model관광지 관광지 = new Model관광지(관광지ID, 코스ID, 지역ID, 관광지명, 경도, 위도, 코스순서, 이동시간, 실내구분, 테마분류);
+                int i = 1;
+                int 관광지ID = resultSet.getInt(i++);
+                String 지역ID = resultSet.getString(i++);
+                String 관광지명 = resultSet.getString(i++);
+                BigDecimal 경도 = resultSet.getBigDecimal(i++);
+                BigDecimal 위도 = resultSet.getBigDecimal(i++);
+                String 실내구분 = resultSet.getString(i++);
+                String 테마분류 = resultSet.getString(i++);
+                Model관광지 관광지 = new Model관광지(관광지ID, 지역ID, 관광지명, 경도, 위도, 실내구분, 테마분류);
                 관광지List.add(관광지);
             }
 
@@ -79,17 +77,15 @@ public class MainRepository {
             ArrayList<Model관광지> 관광지List = new ArrayList<>();
             while (resultSet.next()) {
                 // 각 열의 값을 가져오기 (여기서는 예를 들어 1번째와 2번째 열의 값을 가져옵니다)
-                int 관광지ID = resultSet.getInt(1);
-                int 코스ID = resultSet.getInt(2);
-                String 지역ID = resultSet.getString(3);
-                String 관광지명 = resultSet.getString(4);
-                BigDecimal 경도 = resultSet.getBigDecimal(5);
-                BigDecimal 위도 = resultSet.getBigDecimal(6);
-                int 코스순서 = resultSet.getInt(7);
-                int 이동시간 = resultSet.getInt(8);
-                String 실내구분 = resultSet.getString(9);
-                String 테마분류 = resultSet.getString(10);
-                Model관광지 관광지 = new Model관광지(관광지ID, 코스ID, 지역ID, 관광지명, 경도, 위도, 코스순서, 이동시간, 실내구분, 테마분류);
+                int i = 1;
+                int 관광지ID = resultSet.getInt(i++);
+                String 지역ID = resultSet.getString(i++);
+                String 관광지명 = resultSet.getString(i++);
+                BigDecimal 경도 = resultSet.getBigDecimal(i++);
+                BigDecimal 위도 = resultSet.getBigDecimal(i++);
+                String 실내구분 = resultSet.getString(i++);
+                String 테마분류 = resultSet.getString(i++);
+                Model관광지 관광지 = new Model관광지(관광지ID, 지역ID, 관광지명, 경도, 위도, 실내구분, 테마분류);
                 관광지List.add(관광지);
             }
 
@@ -98,6 +94,124 @@ public class MainRepository {
             statement.close();
 
             return 관광지List;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.releaseConnection(conn);
+        }
+    }
+
+    public ArrayList<Integer> get코스List(boolean isFavorite) {
+        Connection conn = pool.getConnection();
+        if(conn == null) {
+            System.out.println("Failed Get Connection");
+            return null;
+        }
+
+        String query = (isFavorite) ? "SELECT * FROM 선호코스" : "SELECT * FROM 코스";
+
+        // SQL 쿼리 실행
+        Statement statement = null;
+        try {
+            statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM 코스");
+
+            // 결과 출력 및 저장
+            ArrayList<Integer> 코스IDList = new ArrayList<>();
+            while (resultSet.next()) {
+                // 각 열의 값을 가져오기 (여기서는 예를 들어 1번째와 2번째 열의 값을 가져옵니다)
+                int i = 1;
+                int 코스ID = resultSet.getInt(i++);
+                코스IDList.add(코스ID);
+            }
+
+            // 자원 해제
+            resultSet.close();
+            statement.close();
+
+            return 코스IDList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.releaseConnection(conn);
+        }
+    }
+
+    public ArrayList<Integer> get선호코스IDList() {
+        Connection conn = pool.getConnection();
+        if(conn == null) {
+            System.out.println("Failed Get Connection");
+            return null;
+        }
+
+        // SQL 쿼리 실행
+        Statement statement = null;
+        try {
+            statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM 선호코스");
+
+            // 결과 출력 및 저장
+            ArrayList<Integer> 코스IDList = new ArrayList<>();
+            while (resultSet.next()) {
+                // 각 열의 값을 가져오기 (여기서는 예를 들어 1번째와 2번째 열의 값을 가져옵니다)
+                int i = 1;
+                int 코스ID = resultSet.getInt(i++);
+                코스IDList.add(코스ID);
+            }
+
+            // 자원 해제
+            resultSet.close();
+            statement.close();
+
+            return 코스IDList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.releaseConnection(conn);
+        }
+    }
+
+    /**
+     *   되는지 모름
+     *   코스별로 관광지 목록 띄우는건 Service쪽에서 코스정보 모델의 ArrayList 사용해서 할 것
+     */
+    public Model코스정보 get코스정보(int 대상코스ID) {
+        Connection conn = pool.getConnection();
+        if(conn == null) {
+            System.out.println("Failed Get Connection");
+            return null;
+        }
+
+        // SQL 쿼리 실행
+        String query = "SELECT * FROM 코스정보 WHERE 코스ID = ?";
+        PreparedStatement statement = null;
+        try {
+            statement = conn.prepareStatement(query);
+            statement.setInt(1, 대상코스ID);
+            ResultSet resultSet = statement.executeQuery();
+
+            Model코스정보 코스정보 = new Model코스정보(대상코스ID, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+            // 결과 출력 및 저장
+            while (resultSet.next()) {
+                // 각 열의 값을 가져오기 (여기서는 예를 들어 1번째와 2번째 열의 값을 가져옵니다)
+                int i = 1;
+                int 관광지ID = resultSet.getInt(i++);
+                int 코스ID = resultSet.getInt(i++);
+                int 코스순서 = resultSet.getInt(i++);
+                int 이동시간 = resultSet.getInt(i++);
+                코스정보.get관광지ID목록().add(관광지ID);
+                코스정보.get코스순서목록().add(코스순서);
+                코스정보.get이동시간목록().add(이동시간);
+            }
+
+            // 자원 해제
+            resultSet.close();
+            statement.close();
+
+            return 코스정보;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
